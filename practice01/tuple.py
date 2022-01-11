08263709cfcd1fc18324614501aa58894fbe1622
SEOKHOCHOI <talesost@naver.com>
2022-01-11 13:17:41 +0900

    tuple

---
+++ practice01/tuple.py
@@ -0,0 +1,10 @@
+# 튜플(tuple)
+# 값을 바꿀 수 없는 리스트
+# 소괄호 사용
+tuple_a = (1, 2, 3, 4)
+
+# tuple_a[0] = 2 이렇게 0번째를 2로바꾸는
+# 행위를 하면
+# 'tuple' object does not support item assignment
+# 라는 할당할수 없다는 오류가 나와.
+# 즉, 만들고 접근만 가능하고 수정이 불가능해!
