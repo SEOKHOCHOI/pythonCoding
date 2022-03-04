9438100617b14e2fbd6fbff6cbc703311d2e9ab2
SEOKHOCHOI <talesost@naver.com>
2022-03-04 22:33:46 +0900

    - 메모이제이션

--- practiceCT/dynamic_programming/dynamic_programming.py
+++ practiceCT/dynamic_programming/dynamic_programming.py
@@ -102,3 +102,19 @@ for i in range(3, n + 1):
     d2[i] = d[i - 1] + d[i - 2]
 
 print(d2[n])
+
+# 메모이제이션을 이용하는 경우 피보나치 수열 함수의 시간 복잡도는 O(N)입니다.
+d3 = [0] * 100
+
+
+def fibo2(x):
+    print('f(' + str(x) + ')', end=' ')
+    if x == 1 or x == 2:
+        return 1
+    if d3[x] != 0:
+        return d3[x]
+    d3[x] = fibo2(x - 1) + fibo2(x - 2)
+    return d3[x]
+
+
+fibo2(6)
